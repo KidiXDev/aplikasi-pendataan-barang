a5b763b3dcc782d7cf04d4941f42763a2c3a85c7
KidiXDev <dikhiardian55@gmail.com>
2023-12-21 22:04:51 +0700

    fixed connection close issue

--- src/view_models/login_form_view_models.py
+++ src/view_models/login_form_view_models.py
@@ -6,25 +6,25 @@ from str_basic_encryption import StrEncryption
 class LoginFormViewModel:
     def __init__(self):
         self.db = config.dbConfig.db()
-        self.dbc = self.db.cursor()
 
-    def loginAction(self, username, password, menu):
+    def loginAction(self, username, password, ins):
+        dbc = self.db.cursor()
         encryptor = StrEncryption()
         encrypted_password = encryptor.str2md5(password)
 
         query = "SELECT * FROM account WHERE username = %s AND password = %s"
 
         try:
-            self.dbc.execute(query, (username, encrypted_password))
-            result = self.dbc.fetchone()
+            dbc.execute(query, (username, encrypted_password))
+            result = dbc.fetchone()
 
             if result:
                 print("Login successful")
-                menu.loadMainMenu()
+                ins.loadMainMenu()
             else:
                 messagebox.showinfo("Error", "Invalid username or password")
                 print("Login failed")
         except mysql.connector.Error as err:
             print(f"Error: {err}") 
         finally:
-            self.dbc.close()
+            dbc.close()
